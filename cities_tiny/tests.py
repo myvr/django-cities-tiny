0cbe3a5bfc4d9dfc9cb02a173195db9f9894f705
Keith Dechant <kdechant@myvr.com>
2021-08-06 12:18:21 -0700

    Simple code reformatting
    * One import per line
    * Tabs -> Spaces
    * Other automatic whitespace formatting done by PyCharm

--- cities_tiny/tests.py
+++ cities_tiny/tests.py
@@ -1,12 +1,17 @@
 import unittest
+
 from django.core import management
-from cities_tiny.models import Country, AdminDivision, City
+
+from cities_tiny.models import AdminDivision
+from cities_tiny.models import City
+from cities_tiny.models import Country
+
 
 class RefreshCommandTestCase(unittest.TestCase):
     def test_command(self):
-	management.call_command('citiestinyrefresh', verbosity=2,
-		force_import_all=True, force_all=False,
-		force_import=[], force=[])
-	self.assertEqual(Country.objects.count() > 0, True)
-	self.assertEqual(AdminDivision.objects.count() > 0, True)
-	self.assertEqual(City.objects.count() > 0, True)
+        management.call_command('citiestinyrefresh', verbosity=2,
+                                force_import_all=True, force_all=False,
+                                force_import=[], force=[])
+        self.assertEqual(Country.objects.count() > 0, True)
+        self.assertEqual(AdminDivision.objects.count() > 0, True)
+        self.assertEqual(City.objects.count() > 0, True)
